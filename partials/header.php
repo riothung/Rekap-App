929ed041899f9bcf31440706e5970702ca3d0ea3
riothung <mario.thung@gmail.com>
2024-04-21 01:10:07 +0800

    revisi

--- partials/header.php
+++ partials/header.php
@@ -26,7 +26,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Dashboard - NiceAdmin Bootstrap Template</title>
+  <title>Rekap Data</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -67,7 +67,7 @@
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.php" class="logo d-flex align-items-center">
         <img src="assets/img/logo-bawaslu.png" alt="">
-        <span class="d-none d-lg-block">BAWASLU KAB.</span>
+        <span class="d-none d-lg-block">BAWASLU KAB. KUPANG</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
