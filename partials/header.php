eab27d6d69deaa770b8ccab214b615be80d2ccaa
riothung <mario.thung@gmail.com>
2024-04-29 21:27:12 +0800

    just some minor change

--- partials/header.php
+++ partials/header.php
@@ -72,11 +72,8 @@
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
 
-    
-
     <nav class="header-nav ms-auto">
       <ul class="d-flex align-items-center">
-
 
         <li class="nav-item dropdown pe-3">
 
